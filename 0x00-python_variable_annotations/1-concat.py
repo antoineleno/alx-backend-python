b82225c91c532058d36179b44e65f16ac08aef96
antoineleno <johndoe@example.com>
2024-10-10 22:29:55 +0800

    python variable annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat function
+
+    Args:
+        str1 (str): First string
+        str2 (str): Second string
+    """
+    return str1 + str2
