d81b31b30f02092473e89699a0014875e2a8f1ee
antoineleno <johndoe@example.com>
2024-10-10 22:32:52 +0800

    Python variable annotaions

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Floor module"""
+
+
+def floor(n: float) -> float:
+    """floor function
+
+    Args:
+        n (float): Floating number argument
+
+    Returns:
+        float: Floor value of n
+    """
+    return round(n)
+
