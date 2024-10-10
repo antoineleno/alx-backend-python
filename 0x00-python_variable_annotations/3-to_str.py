39648f9310d957db6dc7f7c056ff4c28f05760bf
antoineleno <johndoe@example.com>
2024-10-10 22:49:19 +0800

    Python variable annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""to_str module
+"""
+
+
+def to_str(n: float) -> str:
+    """to_str function
+
+    Args:
+        n (float): Floating number
+
+    Returns:
+        str: string representation of the n
+    """
+    return str(n)
