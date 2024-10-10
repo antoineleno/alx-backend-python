cc161fe067c7c0db8426ca0f39ce684a0b6c003c
antoineleno <johndoe@example.com>
2024-10-10 23:34:59 +0800

    Python variable annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""5-sum_list module"""
+
+
+type type_list = list[float]
+
+
+def sum_list(input_list: type_list) -> float:
+    """summ_list function
+
+    Args:
+        input_list (type_list): a list of numbers
+
+    Returns:
+        float: The sum of all values in the list
+    """
+    return sum(sum_list)
