1487a6b0bd559ec9a328f7ea376c7726749b489a
antoineleno <johndoe@example.com>
2024-10-10 22:26:44 +0800

    variable annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,4 +3,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """add function
+
+    Args:
+        a (float): First variable
+        b (float): second variable
+
+    Returns:
+        float: The sum of a and b
+    """
     return a + b
