fa58b3b89b0d267fe7dc79ee56025d4db659de4b
antoineleno <lenoantoine2000@gmail.com>
2025-06-28 17:40:28 +0800

    Testing with memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ Unit test for utils.get_json
 
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import get_json
+from utils import get_json, memoize
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -56,3 +56,31 @@ class TestGetJson(unittest.TestCase):
 
             # Ensure the result of get_json is test_payload
             self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """Tests for the memoize decorator"""
+
+    def test_memoize(self):
+        """Test that memoize caches the result of a method"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_obj = TestClass()
+
+            # Access the property twice (without calling)
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            # Should return correct result
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            # a_method should only be called once due to caching
+            mock_method.assert_called_once()
